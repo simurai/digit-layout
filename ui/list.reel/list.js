e83ccda76d9e94d16f8896cbb70edc615f17708e
simurai <simulus@gmail.com>
2014-05-31 16:54:36 +0900

    Add List

---
+++ ui/list.reel/list.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/list.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class List
+ * @extends Component
+ */
+exports.List = Component.specialize(/** @lends List# */ {
+    constructor: {
+        value: function List() {
+            this.super();
+        }
+    }
+});
